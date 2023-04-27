d824b6c81b5f142f233476237c4976748f7c7427
Antonio Cruz <antonio.cruz5612@gmail.com>
2023-04-27 14:56:14 -0700

    add simple code to check if num is negative. Else, multiply num by -1 and return num.

---
+++ return-negative.js
@@ -0,0 +1,14 @@
+let num = 10;
+
+function convertToNegative(num) {
+    if (num < 0) {
+        // return statement here
+        console.log(num);
+    } else {
+        num = num * -1
+        // return statement here
+        console.log(num);
+    }
+}
+
+convertToNegative(num);
